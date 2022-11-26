19bbf09d92978f54560d5bdbbd545afd3dc4786b
Santiago Passafiume <santiagopassafiume@protonmail.com>
2022-11-25 23:07:49 -0300

    create promptOptions function

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -18,7 +19,6 @@ func getInput(prompt string, r *bufio.Reader) (string, error) {
 func createBill() bill {
 	reader := bufio.NewReader(os.Stdin)
 
-
 	name, _ := getInput("Create a new bill name: ", reader)
 
 	b := newBill(name)
@@ -27,8 +27,47 @@ func createBill() bill {
 	return b
 }
 
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+	opt, _ := getInput("Choose option (a - add item, s - save bill, t - add tip): ", reader)
+
+	switch strings.ToLower(opt) {
+	case "a":
+		name, _ := getInput("Item name: ", reader)
+		price, _ := getInput("Item price: ", reader)
+
+		p, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			fmt.Println("The price must be a number.")
+			promptOptions(b)
+		}
+		b.addItem(name, p)
+		fmt.Println("Item added -", name, p)
+		promptOptions(b)
+	case "t":
+		tip, _ := getInput("Enter tip amount ($): ", reader)
+
+		t, err := strconv.ParseFloat(tip, 64)
+		if err != nil {
+			fmt.Println("The tip must be a number.")
+			promptOptions(b)
+		}
+		b.updateTip(t)
+
+		fmt.Println("Tip added -", tip)
+		promptOptions(b)
+	case "s":
+		fmt.Println("You chose to save the bill\n", b.format())
+
+	default:
+		fmt.Println("NOT A VALID OPTION")
+		promptOptions(b)
+	}
+
+}
+
 func main() {
 	mybill := createBill()
-	fmt.Println(mybill)
+	promptOptions(mybill)
 
 }
